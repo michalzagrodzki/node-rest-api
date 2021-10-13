1766b6d7efc231c2f1dbe98114cef0a7258768a6
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-10-13 16:21:59 +0200

    feat: add responses to items

--- routes/items.js
+++ routes/items.js
@@ -1,18 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const list = require("./../mockup/items")
+
 /* GET items listing. */
 router.get('/', function(req, res, next) {
-  res.json({
-    title: "example title",
-    description: "example description",
-  });
+  res.status(200).json(list);
 });
 
 /* GET single item. */
 router.get('/:id', function(req, res, next) {
-  const {id} = req.params;
-  res.send("single item");
+  const { id } = req.params;
+  const intId = parseInt(id, 10);
+  const itemById = list.filter(item => item.id === intId)[0];
+  console.log(itemById)
+  if (itemById) {
+    res.status(200).json(itemById);
+  } else {
+    res.status(404).send("Element not found");
+  }
 });
 
 module.exports = router;
