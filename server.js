f919d6334b318cace6d8e041db607a0294038a31
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-10-13 10:31:19 +0200

    fix: fix wrong filepath

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-const app = require('../app');
+const app = require('./app');
 const debug = require('debug')('node-rest-api:server');
 const http = require('http');
 
