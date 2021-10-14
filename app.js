51c1d6df3191cb31e88246bb1340d6bf85ff7565
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-10-14 14:59:24 +0200

    feat: add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require("cors");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -8,7 +9,7 @@ const itemsRouter = require('./routes/items');
 const apiRouter = require("./docs/docs.route");
 
 const app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
